ef2289e99672a32e19636088f47a55a99af45c45
Jose GSS <joseg2s.1995@gmail.com>
2022-12-09 18:55:04 -0600

    Initial Commit

---
+++ OpenTable-Test/OpenTable_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenTable_TestApp.swift
+//  OpenTable-Test
+//
+//  Created by Jose Gutierrez on 09/12/22.
+//
+
+import SwiftUI
+
+@main
+struct OpenTable_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
